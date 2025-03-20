8231b4df2848be92ee0c8105339e65f41b68bc3c
kamilabalabatyr <k_balabatyr@kbtu.kz>
2025-03-21 00:36:13 +0500

    fixed upd

--- lab7.py
+++ lab7.py
@@ -1,53 +1,49 @@
 import pygame
-import datetime
+from datetime import datetime
 
 pygame.init()
 
-width = 400
-height = 400 
-screen = pygame.display.set_mode((width,height))
-pygame.display.set_caption("Часы с руками Микки")
 
-# Загружаем изображения
-right_hand = pygame.image.load('mickeyclock.jpeg') 
-left_hand = pygame.image.load('mickeyclock.jpeg')  
+screen = pygame.display.set_mode((830, 800))
+pygame.display.set_caption("Mickey Clock")
 
-# Функция для вычисления угла поворота руки
-def getrotationangle(time, max):
-    return -360 * (time / max)
 
-# Главный цикл
-running = True
-while running:
-    #текущее время
-    now = datetime.datetime.now()
-    seconds = now.second
-    minutes = now.minute
+bg_image = pygame.image.load('mickeyclock.jpeg') 
+sec_image = pygame.image.load('righthand.PNG')
+min_image = pygame.image.load('lefthand.PNG')
 
-    #углы для вращения рук
-    right_angle = getrotationangle(minutes, 60)
-    left_angle = getrotationangle(seconds, 60)
 
-    # Вращаем руки
-    right_hand_rotate = pygame.transform.rotate(right_hand, right_angle)
-    left_hand_rotate = pygame.transform.rotate(left_hand, left_angle)
+rect = bg_image.get_rect(center=(415, 400))  
 
-    #новые размеры после вращения
-    right_rect = right_hand_rotate.get_rect(center=(width//2,height//2))
-    left_rect = left_hand_rotate.get_rect(center=(width//2, height//2))
 
-    # Заполняем экран белым цветом
-    screen.fill((255, 255, 255))
+clock = pygame.time.Clock()
 
-    # Отображаем руки
-    screen.blit(right_hand_rotate, right_rect)
-    screen.blit(left_hand_rotate, left_rect)
+done = False
+while not done:
+    screen.fill((255, 255, 255))  
+    screen.blit(bg_image, rect.topleft)  
 
-    pygame.display.update()
-
-    # Проверяем, не закрыто ли окно
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            done = True
+
+    
+    time = datetime.now().time()
+
+    
+    sec_angle = -(time.second * 6) + 90  
+    nsec_img = pygame.transform.rotate(sec_image, sec_angle)
+    sec_rect = nsec_img.get_rect(center=rect.center)  
+    screen.blit(nsec_img, sec_rect)
+
+    
+    min_angle = -(time.minute * 6) + 90
+    nmin_img = pygame.transform.rotate(min_image, min_angle)
+    min_rect = nmin_img.get_rect(center=rect.center)
+    screen.blit(nmin_img, min_rect)
+
+    
+    pygame.display.flip()
+    clock.tick(30) 
 
 pygame.quit()
